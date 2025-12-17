c4bb34712456520e56cf40870125ac25fc5be928
Patrik Modesto <patrik.modesto@gmail.com>
2025-12-17 10:10:19 +0100

    Day 4 - part 2

--- src/day4.cc
+++ src/day4.cc
@@ -3,36 +3,20 @@
 #include <fmt/core.h>
 #include <fmt/ranges.h>
 
-#include <algorithm>
-#include <cmath>
 #include <iostream>
-#include <map>
-#include <unordered_map>
 #include <unordered_set>
 
 using Coord = int32_t;
 using Point = Gfx_2d::Point<Coord>;
 
-using Map = std::unordered_map<Point, char, boost::hash<Point>>;
+using Map = std::unordered_set<Point, boost::hash<Point>>;
 
-std::pair<Coord, Coord> measure(Map const& mapa)
-{
-    Coord max_x = std::numeric_limits<Coord>::min();
-    Coord max_y = std::numeric_limits<Coord>::min();
-    for (auto const& [p, _] : mapa) {
-        max_x = std::max(max_x, p.x);
-        max_y = std::max(max_y, p.y);
-    }
-    ++max_x;
-    ++max_y;
-    return {max_x, max_y};
-}
 
 void part1(Map const& world)
 {
     unsigned movable{0};
 
-    for (auto const& [px, _] : world) {
+    for (auto const& px : world) {
         unsigned around{0};
         for (auto const& dir :
              {Gfx_2d::North, Gfx_2d::South, Gfx_2d::West, Gfx_2d::East, Gfx_2d::NW, Gfx_2d::NE, Gfx_2d::SW, Gfx_2d::SE})
@@ -43,12 +27,54 @@ void part1(Map const& world)
         }
 
         if (around < 4) {
-            fmt::print("Movable: ({}, {}) with {} around\n", px.x, px.y, around);
             ++movable;
         }
     }
 
     fmt::print("1: {}\n", movable);
+}
+
+
+void part2(Map world)
+{
+    unsigned removable{0};
+
+    for (;;) {
+        Map candidates;
+
+        for (auto const& px : world) {
+            unsigned around{0};
+            for (auto const& dir :
+                 {Gfx_2d::North,
+                  Gfx_2d::South,
+                  Gfx_2d::West,
+                  Gfx_2d::East,
+                  Gfx_2d::NW,
+                  Gfx_2d::NE,
+                  Gfx_2d::SW,
+                  Gfx_2d::SE})
+            {
+                if (world.contains(px + dir)) {
+                    ++around;
+                }
+            }
+
+            if (around < 4) {
+                ++removable;
+                candidates.insert(px);
+            }
+        }
+
+        if (candidates.empty())
+            break;
+
+        for (auto const& px : candidates) {
+            world.erase(px);
+        }
+        candidates.clear();
+    }
+
+    fmt::print("2: {}\n", removable);
 }
 
 int main()
@@ -64,7 +90,7 @@ int main()
             Coord x{0};
             for (auto const& c : line) {
                 if (c == '@') {
-                    world.insert({{x, y}, c});
+                    world.insert({x, y});
                 }
                 ++x;
             }
@@ -72,6 +98,7 @@ int main()
     }
 
     part1(world);
+    part2(world);
 
     return 0;
 }
