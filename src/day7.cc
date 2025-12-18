c2ead3a5004bffd0a5c2217f518c413bc43ef4d3
Patrik Modesto <patrik.modesto@gmail.com>
2025-12-18 11:15:00 +0100

    Day 7 - part 2

--- src/day7.cc
+++ src/day7.cc
@@ -1,5 +1,6 @@
 #include "point2d.h"
 
+#include <cstdint>
 #include <fmt/core.h>
 
 #include <cassert>
@@ -43,6 +44,46 @@ void part1(Map const& world, Point const& start)
     fmt::print("1: {}\n", splits);
 }
 
+class Part2
+{
+    std::unordered_map<Point, uint64_t> cache;
+    Map const& world;
+
+public:
+    Part2(Map const& w)
+        : world{w}
+    {
+    }
+
+    uint64_t beam(Point const& start)
+    {
+        Point px{start};
+        for (;; px += Gfx_2d::Down) {
+            if (!world.contains(px))
+                return 1;
+
+            if (world.at(px) == '^') {
+                break;
+            }
+        }
+        assert(world.at(px) == '^');
+
+        if (cache.contains(px)) {
+            return cache.at(px);
+        }
+
+        uint64_t v{0};
+        for (auto const& dir : {Gfx_2d::Left, Gfx_2d::Right}) {
+            Point next{px + dir};
+            v += beam(next);
+        }
+
+        cache.insert({px, v});
+        return v;
+    }
+};
+
+
 int main()
 {
     Map world;
@@ -69,6 +110,9 @@ int main()
     }
 
     part1(world, start);
+
+    Part2 d{world};
+    fmt::print("2: {}\n", d.beam(start));
 
     return 0;
 }
